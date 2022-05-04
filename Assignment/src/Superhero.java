e0cd05cf406648a02e31042be8c60273508a7841
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-04 15:58:49 -0400

    corrected version superhero

--- Assignment/src/Superhero.java
+++ Assignment/src/Superhero.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Superhero {
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
-        String superheroName = "Batman";
-        String superPower = "eat stinky cheese";
         System.out.println("Let's write a story about a superhero.");
         System.out.println("Who is the hero of our story?");
-        String response = scanner.nextLine();
+        String superheroName = scanner.nextLine();
         System.out.println("What is their superpower?");
-        String response2 = scanner.nextLine();
+        String superPower = scanner.nextLine();
         System.out.println("Here is the story:");
         System.out.println("There was once a superhero named " + superheroName + " who had the power of " + superPower);
         System.out.println("As they grew older " + superheroName + " saw that the world needed them.");
